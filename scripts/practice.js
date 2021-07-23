11a4aae5f4669a42ea6cd4562191de1e614d6cd0
Ted Marov <ted.marov@gmail.com>
2021-07-23 14:45:39 -0500

    Skeleton

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,9 +1,19 @@
-const calcAgeDogYears = (ageHuman) => {
-    const ageDog = ageHuman / 7
-    return ageDog
+// const calcAgeDogYears = (ageHuman) => {
+//     const ageDog = ageHuman / 7
+//     return ageDog
 
+// }
+
+// // calcAgeDogYears provides an input, so that the console.log has something to show
+// const ageDogAnswer = calcAgeDogYears(14)
+// console.log(ageDogAnswer)
+
+
+
+const favoriteBreed = (breedName) => {
+    const breedName = 
+
+    return "My favorite breed of dog is " ${breedName}
 }
 
-// calcAgeDogYears provides an input, so that the console.log has something to show
-const ageDogAnswer = calcAgeDogYears(14)
-console.log(ageDogAnswer)
+const 
