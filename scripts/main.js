4cca3ed6216a681372f54a9d3036a3b1e58f790b
S. A. Marov <ted.marov@gmail.com>
2021-09-01 13:16:12 -0500

    Breed logic added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 console.log("Welcome to the main module. To access other parts, you need to mod the source.")
+
+const favoriteBreed = (breedName) => {
+    const sentence = `My favorite breed of dog is ${breedName}`
+
+    return sentence
+}
+
+console.log(favoriteBreed("schnauzer"))
